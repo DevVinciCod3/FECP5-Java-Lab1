479e26508b8a373969d385f1417863aebf06980a
Marc Vincent Alcoriza <marcvincent.alcoriza@GXCHLAP00692.local>
2025-07-01 14:47:32 +0800

    Final Submission, Lab 1: Celsius to Fahrenheit Converter

---
+++ Readme.md
@@ -0,0 +1,6 @@
+### Marc Vincent Alcoriza
+
+# Lab 1: Celsius to Fahrenheit Converter
+
+![Screenshot 2025-07-01 at 2.38.44 PM.png](Screenshot%202025-07-01%20at%202.38.44%E2%80%AFPM.png)
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,18 @@
 package org.example;
-
+import java.util.*;
 import java.util.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Input temperature in celcius: ");
+        double cel = s.nextDouble();
+
+        double fahr = cel * (9/5) + 32;
+
+        System.out.println("Temperature in Fahrenheit: " + fahr);
+
 
     }
 }
